453eb54c0278f943160ff421e1cef61d1fe8ac01
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-05-20 16:53:01 -0400

    Only generate a self-signed certificate if one has not already been provided (via Docker volume mount)

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#Setup a self-signed TLS certificate
-mkdir -p /etc/ssl/private
-chmod 700 /etc/ssl/private
-mkdir -p /etc/ssl/certs
-
-echo -ne '\n\n\n\n\n\n\n\n\n\n' | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/selfsigned.key -out /etc/ssl/certs/selfsigned.crt
+if [ ! -f /etc/ssl/private/cert.key ]
+then
+	#Setup a self-signed TLS certificate if no SSL cert/key pair is provided
+	echo -ne '\n\n\n\n\n\n\n\n\n\n' | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/cert.key -out /etc/ssl/certs/cert.crt
+fi
 
 nginx -g 'daemon off;'
