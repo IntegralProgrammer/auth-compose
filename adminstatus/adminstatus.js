1708841fba41affd84d20d963c9aa3256f585d17
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-05-22 16:59:29 -0400

    Add the /adminstatus/confirm HTTP POST endpoint to the back-end

--- adminstatus/adminstatus.js
+++ adminstatus/adminstatus.js
@@ -1,11 +1,25 @@
+const fs = require('fs');
 const http = require('http');
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', function(req, res) {
+app.get('/adminstatus', function(req, res) {
 	res.status(200);
-	res.send("This is the status page for the admin");
+	res.type('html');
+	res.send(fs.readFileSync("/users_table.html"));
+});
+
+app.post('/adminstatus/confirm', function(req, res) {
+	var postData = "";
+	req.on('data', function(d) {
+		postData += d;
+	});
+	req.on('end', function() {
+		console.log("Authorized Users: " + postData);
+		res.status(200);
+		res.send("Authorized users confirmed");
+	});
 });
 
 server.listen(3000, '0.0.0.0');
