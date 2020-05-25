07402c812393d9b8858dc796e264754b18e51feb
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-05-25 18:30:34 -0400

    Add authentication required message to mock service

--- mockservice/mockservice.js
+++ mockservice/mockservice.js
@@ -5,7 +5,12 @@ const server = http.createServer(app);
 
 app.get('*', function(req, res) {
 	res.status(200);
-	res.send("You have issued " + req.method + " to the mock endpoint " + req.url);
+	res.send("You have issued " +
+	  req.method +
+	  " to the mock endpoint "
+	  + req.url
+	  + " IF YOU ARE SEEING THIS WITHOUT HAVING AUTHENTICATED, SOMETHING \
+	  HAS GONE HORRIBLY WRONG");
 });
 
 server.listen(3000, '0.0.0.0');
