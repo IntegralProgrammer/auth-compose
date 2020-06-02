7388d6fd7ca5477eee1333f43ec0525f8f5208db
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-06-02 12:57:12 -0400

    When there are improperly configured users, run render_adminpage.py at high frequency

--- adminstatus/audit_required.sh
+++ adminstatus/audit_required.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd /
-python3 /render_adminpage.py openldap /users_table.html 2>/dev/null >/dev/null
+# If there are improperly configured users, try again once every minute
+# for the next 24 hours (1440 minutes)
+for trial in `seq 1 1440`
+do
+	python3 /render_adminpage.py openldap /users_table.html 2>/dev/null >/dev/null && break
+	sleep 60
+done
 touch /var/usersaudit/review_required
